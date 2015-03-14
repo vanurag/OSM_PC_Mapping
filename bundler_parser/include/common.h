fe34837e675ccd8ecfc2ba8cb103a492d68c24f9
Anurag Vempati <vanurag9@gmail.com>
2015-03-14 21:49:07 +0100

    utils and common libs

---
+++ bundler_parser/include/common.h
@@ -0,0 +1,22 @@
+#ifndef COMMON_H_
+#define COMMON_H_
+
+#include <flags.h>
+
+// STL headers.
+#include <cmath>
+#include <ctype.h>
+#include <iostream>
+#include <numeric>
+#include <string>
+#include <vector>
+
+// Boost
+#include <boost/multi_array.hpp>
+
+// GFlags, GLog and GTest headers.
+#include <gflags/gflags.h>
+#include <glog/logging.h>
+#include <gtest/gtest.h>
+
+#endif // COMMON_H_
